fb2d7927c92514416b971715fd959316c3d26d1b
mkrupczak3 <mkrupczak3@gatech.edu>
2017-10-26 19:23:02 -0400

    some cleanup, added a hamburger and hotdog game runner for debugging purposes

---
+++ hamburger_run_game.bat
@@ -0,0 +1,3 @@
+:: default size 240 160
+:: run a hamburger game, i.e. vertical size halved
+.\halite -d "120 160" "python MyBot.py" "python MyBot1.py"
---
+++ hotdog_run_game.bat
@@ -0,0 +1,3 @@
+:: default size 240 160
+:: run a hotdog game, i.e. vertical size halved
+.\halite -d "240 80" "python MyBot.py" "python MyBot1.py"
